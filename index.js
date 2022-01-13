53b14428795dfaf6275548881a6be234d8e36d21
Gary Barker <garybarker.19894@gmail.com>
2022-01-13 14:46:20 -0500

    All tests passed

--- index.js
+++ index.js
@@ -142,9 +142,11 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
+function getLastReview(array) {
   /*Your code here*/
+  return `${array[array.length - 1].name} gave the restaurant a ${array[array.length - 1].rating} star review, and their feedback was: ${array[array.length - 1].feedback}`
 } 
+getLastReview(reviews);
 
 
 
